1af6cae686ed0bcd5b2c095c20a961cf1663e16d
Julian Schmid <218584912+julian4schmid@users.noreply.github.com>
2025-09-10 20:21:52 +0200

    add getCurrentMonth method

--- src/main/java/io/github/julian4schmid/util/DateUtil.java
+++ src/main/java/io/github/julian4schmid/util/DateUtil.java
@@ -7,15 +7,26 @@ import java.util.List;
 import java.util.Locale;
 
 public class DateUtil {
+    // cwl data only available after the 8th
+    private static final int dayOffset = 8;
+
     public static List<String> getMonths(int n) {
         List<String> months = new ArrayList<>();
 
         LocalDate today = LocalDate.now();
+
+        today = today.minusDays(dayOffset);
         for (int i = 0; i < n; i++) {
             LocalDate date = today.minusMonths(i);
             months.add(date.getMonth().getDisplayName(TextStyle.FULL, Locale.GERMAN));
         }
 
         return months;
+    }
+
+    public static String getCurrentMonth() {
+        LocalDate today = LocalDate.now();
+        today = today.minusDays(dayOffset);
+        return today.getMonth().getDisplayName(TextStyle.FULL, Locale.GERMAN);
     }
 }
